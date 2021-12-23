37c21292a11fbae6b16c7ba6f5e554026d68a030
gabrielcarvajalfigueroa <83176877+gabrielcarvajalfigueroa@users.noreply.github.com>
2021-12-23 19:30:39 -0300

    Add files via upload

---
+++ Codigo/Headers/Historia3.h
@@ -0,0 +1,12 @@
+#pragma once
+class Historia3
+{
+public:
+	/*
+	* Establece inicio y fin del video para poder contar
+	* la cantidad de personas que aparecieron en el video
+	*/
+
+	Historia3();
+};
+
